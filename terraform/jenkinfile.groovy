2d23aa61f05a413bc4225a756631d014de4a8cab
kannan <selvakann@gmail.com>
2018-12-04 17:58:09 +0530

    Update jenkinfile.groovy
    updated

--- terraform/jenkinfile.groovy
+++ terraform/jenkinfile.groovy
@@ -10,16 +10,30 @@ pipeline{
 			      sh 'echo "terraform start....!"'
 			  }
 			}
-		    stage('terraform init'){
+			stage('git clone'){
 			  steps{
-			      sh 'form terraform init'
+			      sh 'sudo git clone https://github.com/selvakann/devops.git'
+			  }
+			}
+		    stage('terraform create'){
+			  steps{
+			      sh 'sudo cp /home/ec2-user/sg.tf terraform init  ./terrajenkins/'
+			  }
+			}
+			stage('terraform init'){
+			  steps{
+			      sh 'sudo /home/ec2-user/terraform init  ./terrajenkins'
 			  }
 			}
 		    stage('terraform plan'){
 			  steps{
-			      sh 'echo "terraform plan....!"'
+			      sh 'sudo /home/ec2-user/terraform plan  ./terrajenkins'
 			  }
 			}
-		
+		    stage('terraform end'){
+			  steps{
+			      sh 'echo "Ended......"'
+			  }
+			}
 	}
 }	
