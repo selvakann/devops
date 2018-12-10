2b4e0bfceb7106b0e25de797c1a14c992dcbedc6
kannan <selvakann@gmail.com>
2018-12-10 18:03:13 +0530

    Delete sg.tf

--- terraform/sg.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_security_group" "web" {
-  name        = "web-access"
-  description = "Security group to allow access from mgmt vpc"
-  vpc_id      = "vpc-e57e399d"
-   tags {
-    Name = "web-access"
-  }
- }
