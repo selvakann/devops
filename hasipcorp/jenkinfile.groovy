43ae552d7cca78d7de74a1ecfd2c8e35b10ede6f
kannan <selvakann@gmail.com>
2018-12-10 15:46:50 +0530

    Create jenkinfile.groovy

---
+++ hasipcorp/jenkinfile.groovy
@@ -0,0 +1,30 @@
+pipeline{
+     agent {
+	     node{
+		  label 'master'
+	 }
+ }
+	  stages {
+		    stage('terraform started'){
+			  steps{
+			      sh 'echo "terraform start....!"'
+			  }
+			}
+			stage('git clone'){
+			  steps{
+			      sh 'sudo rm -rf *;sudo git clone https://github.com/selvakann/devops.git'
+			  }
+			}
+		    stage('terraform init'){
+			  steps{
+			      sh '/var/lib/jenksin/workspace/hasicorp/ terraform init'
+			  }
+			}
+		    stage('terraform plan'){
+			  steps{
+			      sh '/var/lib/jenksin/workspace/hasicorp/ terraform plan'
+			  }
+			}
+		
+	}
+}	
