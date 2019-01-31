4464e786395fe94f538ce9805656ef9d635dd913
selvakann <selvakann@gmail.com>
2019-01-31 20:20:44 +0530

    Update sg.tf

--- hasipcorp/sg.tf
+++ hasipcorp/sg.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group" "web" {
   name        = "web-access"
   description = "Security group to allow access from mgmt vpc"
-  vpc_id      = "vpc-6192bc04"
+  vpc_id      = "vpc-123654788"
    tags {
     Name = "web-access"
   }
